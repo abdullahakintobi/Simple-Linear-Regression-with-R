b9b8cb5ea19c1e31f12c139c599a9c0febd46d31
abdullahakintobi <akintobiabdullah@gmail.com>
2024-11-15 00:02:10 +0100

    Updated R file

--- simple-linear-regression.R
+++ simple-linear-regression.R
@@ -1,1 +1,9 @@
-# Simple Linear Regression with R
+### Simple Linear Regression with R
+
+# Load data into salary_df
+salary_df <- read.csv("Salary_Data.csv", header = TRUE)
+
+# Structure of the data
+str(salary_df)
+
+
