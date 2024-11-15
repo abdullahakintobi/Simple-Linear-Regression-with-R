38dd6d1e807fc236422802b5e9ef87d26dfcf5d2
abdullahakintobi <akintobiabdullah@gmail.com>
2024-11-15 23:58:05 +0100

    Updated R file

--- simple-linear-regression.R
+++ simple-linear-regression.R
@@ -7,4 +7,13 @@ salary_df <- read.csv("Salary_Data.csv", header = TRUE)
 str(salary_df)
 
 # Scatter plot
-plot(salary_df$YearsExperience ~ salary_df$Salary)
+plot(salary_df$YearsExperience, salary_df$Salary)
+
+# Correlation
+cor(salary_df$YearsExperience, salary_df$Salary)
+
+# Fitting linear model
+l_model = lm(YearsExperience ~ Salary, data = salary_df)
+l_model
+
+summary(l_model)
